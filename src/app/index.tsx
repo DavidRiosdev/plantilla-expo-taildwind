fec9fcb04822fe78786b5bce0c0c75541796323f
David Rios <drios@grupovortex.cl>
2025-10-24 11:00:29 -0300

    up add bottomsheet

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,13 @@
-import { StatusBar } from "expo-status-bar";
+import { BottomSheetCustom } from "@/components/ui/BottomSheetCustom";
 import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-xl">Color scheme: claro por defecto</Text>
-      <StatusBar />
+    <View className="flex-1 bg-gray-400 justify-between pt-10">
+      <Text>Hola mundo</Text>
+      <View className="h-[50%] bg-transparent">
+        <BottomSheetCustom />
+      </View>
     </View>
   );
 }
---
+++ src/components/ui/BottomSheetCustom.tsx
@@ -0,0 +1,41 @@
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
+import React, { useCallback, useRef } from "react";
+import { Button, Text } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+export const BottomSheetCustom = () => {
+  // ref
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  // callbacks
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+
+  return (
+    <GestureHandlerRootView className="flex-1">
+      <BottomSheetModalProvider>
+        <Button
+          onPress={handlePresentModalPress}
+          title="Present Modal"
+          color="black"
+        />
+        <BottomSheetModal
+          ref={bottomSheetModalRef}
+          onChange={handleSheetChanges}
+        >
+          <BottomSheetView className="flex-1 bg-gray-800 p-9 items-center w-full">
+            <Text className="text-white">Awesome 🎉</Text>
+          </BottomSheetView>
+        </BottomSheetModal>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
+  );
+};
