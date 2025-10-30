7729e7154b2193053fe76e53ef6d3bcb673745a8
LilHeroz <tmejias@grupovortex.cl>
2025-10-30 15:22:09 -0300

    up

--- src/components/ui/design/CustomButton.tsx
+++ src/components/ui/design/CustomButton.tsx
@@ -1,10 +1,14 @@
+import { Colors } from "@/constants/Colors";
 import * as React from "react";
 import { Button, ButtonProps } from "react-native-paper";
 
 export default function CustomButton(props: ButtonProps) {
   return (
     <Button
-      style={{ borderRadius: 10 }}
+      style={{
+        borderRadius: 10,
+        borderColor: props.mode === "outlined" ? Colors.primary : "",
+      }}
       labelStyle={{ fontSize: 16, fontWeight: "600" }}
       className="px-4 py-1"
       {...props}
